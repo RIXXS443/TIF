9570e8dfd18f1dc657974bbe64c5a06a7cf48b48
RIXXS443 <rortigoza@users.noreply.github.com>
2026-03-15 15:07:58 -0300

    Support both GitHub Pages and Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/TIF/",
+export default defineConfig(() => {
+  const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+  const isCloudflarePages = process.env.CF_PAGES === "1";
+
+  return {
+    base: isGitHubPages && !isCloudflarePages ? "/TIF/" : "/",
+  };
 });
